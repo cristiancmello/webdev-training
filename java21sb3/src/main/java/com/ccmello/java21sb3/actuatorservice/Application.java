d5cab53f4ca9f6f0ea0ae20142a49e4f679ca2c0
Cristian Mello <cristianc.mello@gmail.com>
2024-04-18 20:46:21 -0300

    feat: adding actuator

---
+++ java21sb3/src/main/java/com/ccmello/java21sb3/actuatorservice/Application.java
@@ -0,0 +1,11 @@
+package com.ccmello.java21sb3.actuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String... args) {
+    SpringApplication.run(Application.class);
+  }
+}
